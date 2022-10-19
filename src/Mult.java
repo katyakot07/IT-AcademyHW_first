4232ea54908de7711630a694e407001910cf8fb0
Ekaterina <ekaterina@MacBook-Pro-Ekaterina.local>
2022-10-19 21:53:34 +0300

    Initial commit

---
+++ src/Mult.java
@@ -0,0 +1,11 @@
+public class Mult {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+
+        System.out.println("Произведение чисел:" + a*b*c*d);
+
+    }
+}
